5f745f107cc0bdc2dedfadcae00bdfe4c70a29a1
Ryan McCord <ryan.mccord07@gmail.com>
2024-12-01 09:52:40 -0500

    fix: path handling for P4 where command

--- lua/p4/core/lib/command/where.lua
+++ lua/p4/core/lib/command/where.lua
@@ -1,13 +1,11 @@
 local log = require("p4.log")
 
---- @class P4_Where : table
---- @field clientFile string Local path to the file in local syntax
---- @field DepotFile string Depot path to the file
---- @field path string Local path to the file
-
 --- @class P4_Command_Where_Options : table
 
---- @class P4_Command_Where_Result : P4_Where
+--- @class P4_Command_Where_Result : table
+--- @field client string Local path to the file in local syntax
+--- @field depot string Depot path to the file
+--- @field host string Local path to the file
 
 --- @class P4_Command_Where : P4_Command
 --- @field opts P4_Command_Where_Options Command options.
@@ -19,6 +17,9 @@ local P4_Command_Where = {}
 --- @param opts? P4_Command_Where_Options P4 command options.
 --- @return P4_Command_Where P4_Command_Where P4 command.
 function P4_Command_Where:new(file_paths, opts)
+
+  log.trace("P4 Command Where: new")
+
   opts = opts or {}
 
   P4_Command_Where.__index = P4_Command_Where
@@ -49,16 +50,16 @@ end
 --- Parses the output of the P4 command.
 ---
 --- @param output string Command output.
---- @param p4_file_list? P4_File[] Optional list of P4 files to update in place.
 --- @return P4_Command_Where_Result[] result Hold's the parsed result from the command output.
-function P4_Command_Where:process_response(output, p4_file_list)
+function P4_Command_Where:process_response(output)
+
+  log.trace("P4 Command Where: process_response")
 
   --- @type P4_Command_Where_Result[]
-  local result = {}
+  local result_list = {}
 
-  for index, file_paths in ipairs(vim.split(output, "\n")) do
+  for _, file_paths in ipairs(vim.split(output, "\n")) do
 
-    -- Result contains "depot_path client_path file_path"
     local chunks = {}
 
     -- Convert to table
@@ -66,21 +67,17 @@ function P4_Command_Where:process_response(output, p4_file_list)
       table.insert(chunks, string)
     end
 
-    --- @type P4_File_Path
-    local path = {
+    --- @type P4_Command_Where_Result
+    local result = {
       depot = chunks[1],
       client = chunks[2],
       host = chunks[3],
     }
 
-    result[index] = path
-
-    if p4_file_list then
-      p4_file_list[index].path = path
-    end
+    table.insert(result_list, result)
   end
 
-  return result
+  return result_list
 end
 
 return P4_Command_Where
