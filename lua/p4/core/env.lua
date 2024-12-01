7ccefb35a5b7f3f14a2afe82d3077119b453f13b
Ryan McCord <ryan.mccord07@gmail.com>
2024-12-01 11:50:25 -0500

    chore: move file autocmds from API to core.

--- lua/p4/core/env.lua
+++ lua/p4/core/env.lua
@@ -1,5 +1,3 @@
-local nio = require("nio")
-
 local log = require("p4.log")
 local notify = require("p4.notify")
 
@@ -10,11 +8,13 @@ local config = require("p4.core.config")
 ---@field host? string Identifies the P4 host
 ---@field port? string Identifies the P4 port
 ---@field client? string Identifies the P4 client
+---@field private ac_group? integer Identifies the P4 client
 local P4_Env = {
   user = nil,
   host = nil,
   port = nil,
   client = nil,
+  ac_group = nil,
 }
 
 --- Checks to make sure the P4 enviroment is configured.
@@ -85,6 +85,131 @@ local function update_from_file(config_path)
   end
 end
 
+--- Prompts the user to open the file for add.
+local function prompt_open_for_add(file_path)
+
+    if P4_Env.check() then
+
+      vim.fn.inputsave()
+      local result = vim.fn.input("Open for add (y/n): ")
+      vim.fn.inputrestore()
+
+      if result == "y" or result == "Y" then
+        local P4_File_API = require("p4.api.file")
+
+        P4_File_API.add(file_path)
+      end
+    end
+end
+
+--- Prompts the user to open the file for edit.
+local function prompt_open_for_edit(file_path)
+
+    if P4_Env.check() then
+
+      -- Prevent changing read only warning
+      vim.api.nvim_set_option_value("readonly", false, { scope = "local" })
+
+      vim.fn.inputsave()
+      local opts = {prompt = '[P4] Open file for edit (y/n): ' }
+      local _, result = pcall(vim.fn.input, opts)
+      vim.fn.inputrestore()
+
+      if result == "y" or result == "Y" then
+        local P4_File_API = require("p4.api.file")
+
+        P4_File_API.edit(file_path)
+      else
+        vim.api.nvim_set_option_value("modifiable", false, { scope = "local" })
+
+        -- Exit insert mode
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), 'm', false)
+      end
+    end
+end
+
+--- Enables autocmds
+---
+local function enable_autocmds()
+
+  P4_Env.ac_group = vim.api.nvim_create_augroup("P4_File", {})
+
+  --- Check for P4 workspace when buffer is entered.
+  ---
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = P4_Env.ac_group,
+    pattern = "*",
+    callback = function()
+
+      if P4_Env.update() then
+
+        -- Set buffer to reload for changes made outside vim such as
+        -- pulling latest revisions.
+        vim.api.nvim_set_option_value("autoread", false, { scope = "local" })
+
+      end
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("BufNewFile", {
+    group = P4_Env.ac_group,
+    pattern = "*",
+    callback = function()
+      prompt_open_for_add(vim.fn.expand("%:p"))
+    end,
+  })
+
+  --- If the buffer is written, then prompt the user whether they want
+  --- the associated file opened for add/edit in the client workspace.
+  ---
+  vim.api.nvim_create_autocmd("BufWrite", {
+    group = P4_Env.ac_group,
+    pattern = "*",
+    callback = function()
+      if P4_Env.update() then
+        local file_path = vim.fn.expand("%:p")
+        local modifiable = vim.api.nvim_get_option_value("modifiable", { scope = "local" })
+
+        if not modifiable then
+
+          if vim.fn.filereadable(file_path) then
+
+            prompt_open_for_edit(file_path)
+
+          else
+            prompt_open_for_add(file_path)
+          end
+        end
+      end
+    end,
+  })
+
+  --- If the buffer is modified and read only, then prompt the user
+  --- whether they want the associated file opened for edit in the
+  --- client workspace.
+  ---
+  vim.api.nvim_create_autocmd("FileChangedRO", {
+    group = P4_Env.ac_group,
+    pattern = "*",
+    callback = function()
+        prompt_open_for_edit(vim.fn.expand("%:p"))
+    end,
+  })
+end
+
+--- Disables autocmds
+---
+local function disable_autocmds()
+
+  if P4_Env.ac_group then
+
+     -- Remove file autocmds
+    vim.api.nvim_del_augroup_by_id(P4_Env.ac_group)
+
+    P4_Env.ac_group = nil
+  end
+end
+
 --- Clears the P4 environment information
 function P4_Env.clear()
 
@@ -149,7 +274,7 @@ function P4_Env.update()
       log.debug("P4 configured")
 
       -- Enable autocmds
-      require("p4.api.file.auto").enable_autocmds()
+      enable_autocmds()
 
       local P4_Current_Client = require("p4.core.lib.current_client")
 
@@ -160,7 +285,7 @@ function P4_Env.update()
     else
 
       -- Disable autocmds
-      require("p4.api.file.auto").disable_autocmds()
+      disable_autocmds()
 
       -- Update the current client.
       if not p4.current_client or p4.current_client.name ~= P4_Env.client then
@@ -169,7 +294,7 @@ function P4_Env.update()
 
       -- If nothing is configured, then we will assume this is
       -- not a P4 workspace.
-      if P4_Env.user or P4_Env.host or P4_Env.port or P4_Env.cient then
+      if P4_Env.user or P4_Env.host or P4_Env.port or P4_Env.client then
 
         -- Inform the user what has not been set.
         if not P4_Env.host then
@@ -205,7 +330,7 @@ function P4_Env.update()
     end
   end
 
-  return P4_Env.valid
+  return check_env()
 end
 
 --- Clears the P4 environment information
