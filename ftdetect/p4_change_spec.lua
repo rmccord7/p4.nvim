d30e62e3f9a447d9c7faad2c20b26eac79e1c09d
Ryan McCord <ryan.mccord07@gmail.com>
2026-07-14 11:06:42 -0400

    chore: update ft for p4_change_spec

--- ftdetect/p4_change_spec.lua
+++ ftdetect/p4_change_spec.lua
@@ -4,8 +4,8 @@ vim.filetype.add(
       [".*"] = function(_, bufnr)
         local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
 
-        if vim.regex([[^# A Perforce]]):match_str(content) ~= nil then
-          return "p4_spec"
+        if vim.regex([[^# A Perforce Change Specification]]):match_str(content) ~= nil then
+          return "p4_change_spec"
         end
       end,
     }
